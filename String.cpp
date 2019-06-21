666d9e0253cac80834266bb25baabb0da8c74b9e
Jason Turley <jasonturley0@gmail.com>
2019-06-21 12:04:54 -0500

    add copy constructor

--- String.cpp
+++ String.cpp
@@ -1,50 +1,64 @@
 #include <iostream>
+#include <cstring>
 #include <cstddef>      // size_t
+
+using std::cout;
+using std::endl;
 
 /**
  * Representation of a string.
  */
 class String {
 public:
-        String(char *s, size_t len)
+        /**
+         * Constructor.
+         */
+        String(char *string)
         {
-                str = new char[len+1];  // need room for string and null byte
-                length = len;
-
-                write(s, len);
+                m_size = strlen(string);
+                m_string = new char[m_size+1];
+                strncpy(m_string, string, m_size);
         }
 
+        /**
+         * Copy constructor.
+         */
+        String(const String& old)
+        {
+                m_size = old.m_size;
+                m_string = new char [m_size+1];
+                strncpy(m_string, old.m_string, m_size);
+        }
+
+        /**
+         * Destructor.
+         */
         ~String()
         {
-                delete[] str;
+                delete[] m_string;
         }
 
-        char *read() const { return str; }
-
-        size_t strlen() const { return length; }
+        /**
+         * Prints string to cout.
+         */
+        void print()
+        {
+                cout << m_string << endl;
+        }
 
 private:
-        char *str;
-        size_t length;
-
-        void write(char *s, size_t len)
-        {
-                for (size_t i = 0; i < len; i++)
-                        str[i] = s[i];
-
-                str[len] = '\0';
-        }
+        char *m_string;
+        size_t m_size;
 };
 
 
 int main()
 {
-        String s1 {"Hello", 5};
-        String s2 {"Waterfall Rapids", 16};
+        String s1("HELLO");
+        String s2 = s1;
 
-        std::cout << s1.read() << " "<< s1.strlen() << std::endl;
-        std::cout << s2.read() << " "<< s2.strlen() << std::endl;
+        s1.print();
+        s2.print();
 
         return 0;
-
 }
